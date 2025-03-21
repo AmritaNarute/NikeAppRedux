2d546cc4191f844a1f02ce34df0fece797c98c27
DWebPixel <amruta.narute@dwebpixel.com>
2025-03-21 16:10:37 +0530

    Added dummy  Data files in src folder

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={{ uri: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/nike/nike1.png" }}
+      style={styles.image} />
       
     </View>
   );
@@ -17,4 +18,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  image:{
+    width:'100%',
+    aspectRatio:1,
+  }
 });
