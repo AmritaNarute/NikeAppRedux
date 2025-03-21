af64b7940a9840d950575c09140231453100345f
DWebPixel <amruta.narute@dwebpixel.com>
2025-03-21 17:27:00 +0530

    Product description screen added

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 
 import { StyleSheet, Text, View, Image, FlatList } from 'react-native';
 import ProductsScreen from './src/screens/ProductsScreen';
+import ProductDetailsScreen from './src/screens/ProductDetailsScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <ProductsScreen />
+      {/* <ProductsScreen /> */}
+      <ProductDetailsScreen />
     </View>
   );
 }
@@ -14,8 +16,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    
   },
   
 });
---
+++ src/screens/ProductDetailsScreen.js
@@ -0,0 +1,50 @@
+import { StyleSheet, Text, View, Image, FlatList, useWindowDimensions, ScrollView } from 'react-native'
+import React from 'react'
+import products from '../data/products'
+
+const ProductDetailsScreen = () => {
+    const product = products[0];
+    const { width } = useWindowDimensions();
+
+    return (
+        <View>
+            <ScrollView>
+                <FlatList
+                    data={product.images}
+                    renderItem={({ item }) => (
+
+                        <Image source={{ uri: item }} style={{ width, aspectRatio: 1 }} />
+                    )}
+                    horizontal
+                    showsHorizontalScrollIndicator={false}
+                    pagingEnabled
+                />
+                <View style={{ padding: 20 }}>
+                    <Text style={styles.title}>{product.name}</Text>
+                    <Text style={styles.price}>${product.price}</Text>
+                    <Text style={styles.description}>{product.description}</Text>
+                </View>
+            </ScrollView>
+        </View>
+    )
+}
+
+export default ProductDetailsScreen
+
+const styles = StyleSheet.create({
+    title: {
+        fontSize: 34,
+        fontWeight: '500',
+        marginVertical: 10,
+    },
+    price: {
+        fontWeight: '500',
+        fontSize: 16
+    },
+    description: {
+        marginVertical: 10,
+        fontSize: 18,
+        lineHeight: 28,
+        fontWeight: '300'
+    }
+})
