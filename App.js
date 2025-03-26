a216e6a903d7fc4ad37422a169cf5017b58d1413
DWebPixel <amruta.narute@dwebpixel.com>
2025-03-26 16:10:20 +0530

    added redux to manage store and passing state globally

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 
 import { StyleSheet, Text, View, Image, FlatList } from 'react-native';
 import Navigation from './src/navigation';
+import { Provider } from 'react-redux';
+import { store } from './src/store';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Navigation />
-    </View>
+    <Provider store={store}>
+      <View style={styles.container}>
+        <Navigation />
+      </View>
+    </Provider>
   );
 }
 
@@ -14,7 +18,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    
+
   },
-  
+
 });
--- src/screens/ProductsScreen.js
+++ src/screens/ProductsScreen.js
@@ -1,31 +1,52 @@
-import { StyleSheet, Text, View, Image, FlatList, Pressable } from 'react-native';
-import products from '../data/products'
+import { useNavigation } from '@react-navigation/native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  FlatList,
+  Pressable,
+} from 'react-native';
+import { useSelector, useDispatch } from 'react-redux';
+ import { productSlice } from '../store/productSlice';
 
 const ProductsScreen = ({ navigation }) => {
+  // const navigation = useNavigation();
+
+  const dispatch = useDispatch();
+
+  const products = useSelector((state) => state.products.products);
+
   return (
     <FlatList
       data={products}
       renderItem={({ item }) => (
-        <Pressable onPress={() => navigation.navigate('Product Details')} style={styles.itemContainer}>
+        <Pressable
+          onPress={() => {
+            // update selected product
+            dispatch(productSlice.actions.setSelectedProduct(item.id));
+
+            navigation.navigate('Product Details');
+          }}
+          style={styles.itemContainer}
+        >
           <Image source={{ uri: item.image }} style={styles.image} />
         </Pressable>
       )}
       numColumns={2}
-    //  initialNumToRender={2}
     />
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
-
   itemContainer: {
     width: '50%',
-    padding: 1
+    padding: 1,
   },
   image: {
     width: '100%',
     aspectRatio: 1,
-  }
+  },
 });
 
-export default ProductsScreen
+export default ProductsScreen;
